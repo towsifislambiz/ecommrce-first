f22260bc51fbab580b4ae61d8640cde0bd5ffa2b
Towsifislambiz <towsifislambiz@gmail.com>
2026-05-19 23:36:56 +0600

    fix routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import ContactPart from "./Pages/ContactPart";
-import SingUp from "./Pages/SingUp";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import ContactPart from "./Pages/ContactPart.jsx";
+import SignUp from "./Pages/SignUp.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
+    <>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/ContactPart" element={<ContactPart />} />
-      <Route path="/signup" element={<SingUp />} />
-    </Route>
+      <Route path="/contact" element={<ContactPart />} />
+      <Route path="/signup" element={<SignUp />} />
+    </>
   )
 );
 
